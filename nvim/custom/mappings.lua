961db23f1320f4f38bd0bda041a5d2bb164d4680
ChanghwaPark <omega6464@gmail.com>
2023-03-01 01:18:46 +0900

    Added some useful mappings

--- nvim/custom/mappings.lua
+++ nvim/custom/mappings.lua
@@ -7,20 +7,54 @@ M.disabled = {
 		["<C-l>"] = "",
 		["<C-j>"] = "",
 		["<C-k>"] = "",
+		["d]"] = "",
 	},
 }
 
 M.general = {
 	n = {
+		-- Enter command line
 		[";"] = { ":", "enter command line", opts = { nowait = true } },
+
+		-- Scroll cursor center
 		["<C-u>"] = { "<C-u>zz", "scroll upwards and center the cursor", opts = { nowait = true } },
 		["<C-d>"] = { "<C-d>zz", "scroll downwards and center the cursor", opts = { nowait = true } },
+
+		-- Search cursor center
+		["n"] = { "nzzzv", "search next and center the cursor" },
+		["N"] = { "Nzzzv", "search previous and center the cursor" },
+
+		["x"] = { '"_x', "" },
+
+		-- Increment/decrement
+		["+"] = { "<C-a>", "increment" },
+		["-"] = { "<C-x>", "decrement" },
+
+		-- Split window
+		["ss"] = { ":split<Return>", "horizontal split" },
+		["sv"] = { ":vsplit<Return>", "vertical split" },
+
+		-- Move window
+		["sh"] = { "<C-w>h", "move left" },
+		["sj"] = { "<C-w>j", "move down" },
+		["sk"] = { "<C-w>k", "move up" },
+		["sl"] = { "<C-w>l", "move right" },
 	},
+
 	i = {
 		["jk"] = { "<ESC>", "escape insert mode", opts = { nowait = true } },
 	},
 }
 
--- more keybinds!
+M.lspconfig = {
+	n = {
+		["]d"] = {
+			function()
+				vim.diagnostic.goto_next()
+			end,
+			"goto_next",
+		},
+	},
+}
 
 return M
